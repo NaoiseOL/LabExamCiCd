b0ba4d24ec247b2350b3051374f2a19de38843f4
NaoiseOL <G00385498@atu.ie>
2024-11-05 11:22:58 +0000

    Added Basic Validation Functionality

--- src/main/java/ie/atu/labexam/Product.java
+++ src/main/java/ie/atu/labexam/Product.java
@@ -1,26 +1,30 @@
 package ie.atu.labexam;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldNameConstants;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
 
-
+    @NotBlank(message="Product code cannot be blank")
     private String productCode;
 
+    @NotBlank
     private String productName;
 
+    @NotBlank(message = "Must have a category")
     private String category;
 
+    @Min(0)
     private int quantity;
 
+    @Min(1)
     private int price;
 
 }
