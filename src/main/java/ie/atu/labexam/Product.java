890313cb67cad814077cbaeb96bd3474cf14d0af
NaoiseOL <G00385498@atu.ie>
2024-11-05 10:56:59 +0000

    Added DELETE endpoint

--- src/main/java/ie/atu/labexam/Product.java
+++ src/main/java/ie/atu/labexam/Product.java
@@ -2,6 +2,7 @@ package ie.atu.labexam;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
+
+
     private String productCode;
 
     private String productName;
